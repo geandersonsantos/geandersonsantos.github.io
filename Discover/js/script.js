fa49d30f4392fc4a1f0c681ce3f8f1303f0e41d7
Geanderson Santos <geandersonvelo13@gmail.com>
2025-05-12 15:20:51 -0300

    update add funcionalidade ao popup com js

--- Discover/js/script.js
+++ Discover/js/script.js
@@ -1,5 +1,8 @@
 const Modal = {
             open(){
-                document.querySelector('.modal-overlay')
+                document.querySelector('.modal-overlay').classList.add('active')
+            },
+            close(){
+                document.querySelector('.modal-overlay').classList.remove('active')
             }
         }
